c3d32d54fd1d9b50c546edd5bf33dd1e9865d8cf
hjjang511 <gvu0326@gmail.com>
2025-09-03 16:51:56 +0700

    update Map

--- resource/layout.js
+++ resource/layout.js
@@ -1,7 +1,7 @@
 const layoutData = {
   mapSize: [300, 400],
   gridSize: 50,
-  start: [0, 0],
+  start: [50, 0],
   shelves: [
     { id: "A1", x: 100, y: 150, width: 50, height: 100 },
     { id: "A2", x: 150, y: 150, width: 50, height: 100 },
